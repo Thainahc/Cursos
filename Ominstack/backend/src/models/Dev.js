3838a89144b7dd980e68b9dab74a4bb2bc098822
Thainá Helena <thainahc3@gmail.com>
2019-08-30 12:54:20 -0300

    Inclusão do curso

---
+++ Ominstack/backend/src/models/Dev.js
@@ -0,0 +1,21 @@
+const { Schema, model } = require('mongoose'); // Desestruturação
+
+const DevSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    user: {
+        type: String,
+        required: true
+    },
+    bio: String,
+    avatar: {
+        type: String,
+        required: true
+    },
+}, {
+        timestamps: true, // Criará duas colunas (createdAt e updatedAt), em cada registro
+    });
+
+module.exports = ('Dev', DevSchema);
