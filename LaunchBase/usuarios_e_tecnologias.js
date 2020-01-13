695a06857069a1c290967b38b67b478396c7c82b
Thainá <thainahc3@gmail.com>
2020-01-13 19:44:50 -0300

    Desafio usuários e tecnologias

---
+++ LaunchBase/usuarios_e_tecnologias.js
@@ -0,0 +1,42 @@
+const usuarios = [
+    {
+        nome: 'Thaina',
+        tecnologias: ['xHarbour', 'HTML', 'CSS', 'C#']
+    },
+    {
+        nome: 'Pudim',
+        tecnologias: ['C#', 'HTML', 'CSS', 'JavaScript']
+    },
+    {
+        nome: 'Rafael',
+        tecnologias: ['ReactNative', 'JavaScript']
+    }
+]
+
+for (let index = 0; index < usuarios.length; index++) {
+
+    let usuarioNome = usuarios[index].nome
+    let usuarioTecnologias = usuarios[index].tecnologias
+    let mensagem = `${usuarioNome} trabalha com ${usuarioTecnologias}`
+
+    console.log(mensagem)
+
+    if (checaSeUsuarioUsaCSS(usuarioTecnologias)) {
+        console.log(`${usuarioNome} usa CSS.`)
+    }
+    else {
+        console.log(`${usuarioNome} não usa CSS.`)
+    }
+}
+
+function checaSeUsuarioUsaCSS(usuarioTecnologias) {
+    let usuarioUsaCSS = false;
+
+    for (let index = 0; index < usuarioTecnologias.length; index++) {
+        if (usuarioTecnologias[index] == 'CSS') {
+            usuarioUsaCSS = true
+        }
+    }
+
+    return usuarioUsaCSS
+}
